1fd68faf3510637d0f1062dffabf7f43354694ac
kaz-toc <kaz@favee.net>
2024-04-20 22:58:53 +0900

    add test

--- contracts/CastyToken.sol
+++ contracts/CastyToken.sol
@@ -9,13 +9,60 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CastyToken is ERC20, ERC20Burnable, ERC20Pausable, ERC20Permit, ERC20Votes, Ownable {
+interface ICastyToken {
+  function pause() external;
+  function unpause() external;
+  function mint(address to, uint256 amount) external;
+  function nonces(address owner) external view returns (uint256);
+  function mintableAmount() external view returns (uint256);
+}
+
+contract CastyToken is
+  ICastyToken,
+  ERC20,
+  ERC20Burnable,
+  ERC20Pausable,
+  ERC20Permit,
+  ERC20Votes,
+  Ownable
+{
   string public constant TOKEN_NAME = "CastyToken";
   string public constant TOKEN_SYMBOL = "TY";
-  uint256 public constant TOKEN_INITIAL_SUPPLY = 10_000_000_000;
+
+  uint256 public constant MINIMUM_TIME_BETWEEN_MINTS = 1 days * 30;
+  uint256 public constant PRIMARY_MINT_CAP = 2;
+  uint256 public constant SECONDARY_MINT_CAP = 1;
+  uint256 public constant PRIMARY_MINT_TIMES = 10;
+  uint256 public constant SECONDARY_MINT_TIMES = 20;
+
+  // @dev MAX_SUPPLY is calculated as follows:
+  //   INITIAL_SUPPLY *
+  //     (1 + (PRIMARY_MINT_CAP * PRIMARY_MINT_TIMES + SECONDARY_MINT_CAP * SECONDARY_MINT_TIMES) / 100);
+  uint256 public constant MAX_SUPPLY = 14_000_000_000;
+  uint256 public constant INITIAL_SUPPLY = 10_000_000_000;
+
+  uint256 private _mintedCount = 0;
+  uint256 private _initialSuppliedAt = 0;
+
+  error ZeroAddressBlocked();
+  error MintExceedsMintableAmount();
+  error MintExceedsMaxSupply();
+
+  event Minted(address indexed to, uint256 amount);
+
+  /**
+   * @dev Throws if the address is zero.
+   */
+  modifier nonZeroAddress(address addr) {
+    if (addr == address(0)) {
+      revert ZeroAddressBlocked();
+    }
+    _;
+  }
 
   constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) ERC20Permit(TOKEN_NAME) Ownable(msg.sender) {
-    _mint(msg.sender, TOKEN_INITIAL_SUPPLY * 10 ** decimals());
+    _mint(msg.sender, INITIAL_SUPPLY * 10 ** decimals());
+    _initialSuppliedAt = block.timestamp;
   }
 
   function pause() public onlyOwner {
@@ -26,19 +73,56 @@ contract CastyToken is ERC20, ERC20Burnable, ERC20Pausable, ERC20Permit, ERC20Vo
     _unpause();
   }
 
-  function mint(address to, uint256 amount) public onlyOwner {
-    _mint(to, amount);
+  function mint(address to_, uint256 amount_) public onlyOwner nonZeroAddress(to_) {
+    if (mintableAmount() > amount_) {
+      revert MintExceedsMintableAmount();
+    }
+
+    _mintToken(to_, amount_);
+    _mintedCount += 1;
+
+    emit Minted(to_, amount_);
   }
 
-  function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
-    return super.nonces(owner);
+  function nonces(
+    address owner_
+  ) public view override(ICastyToken, ERC20Permit, Nonces) returns (uint256) {
+    return super.nonces(owner_);
+  }
+
+  function mintableAmount() public view returns (uint256) {
+    uint256 mintableTimes_ = (block.timestamp - _initialSuppliedAt) / MINIMUM_TIME_BETWEEN_MINTS;
+    uint256 primaryCount = mintableTimes_;
+    uint256 secondaryCount = 0;
+
+    if (mintableTimes_ >= PRIMARY_MINT_TIMES) {
+      primaryCount = PRIMARY_MINT_TIMES;
+      secondaryCount = mintableTimes_ - PRIMARY_MINT_TIMES;
+    }
+    if (secondaryCount >= SECONDARY_MINT_TIMES) {
+      secondaryCount = SECONDARY_MINT_TIMES;
+    }
+
+    uint256 primary = primaryCount * PRIMARY_MINT_CAP * INITIAL_SUPPLY;
+    uint256 secondary = secondaryCount * SECONDARY_MINT_CAP * INITIAL_SUPPLY;
+    uint256 suppliable = INITIAL_SUPPLY + (primary + secondary) / 100;
+
+    return suppliable * 10 ** decimals() - totalSupply();
+  }
+
+  function _mintToken(address to_, uint256 amount_) internal {
+    if (totalSupply() + amount_ > MAX_SUPPLY * 10 ** decimals()) {
+      revert MintExceedsMaxSupply();
+    }
+
+    super._mint(to_, amount_);
   }
 
   function _update(
-    address from,
-    address to,
-    uint256 value
+    address from_,
+    address to_,
+    uint256 value_
   ) internal override(ERC20, ERC20Pausable, ERC20Votes) {
-    super._update(from, to, value);
+    super._update(from_, to_, value_);
   }
 }
--- test/CastyToken.ts
+++ test/CastyToken.ts
@@ -1,15 +1,67 @@
 import { ethers } from "hardhat"
 import { expect } from "chai"
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers"
-import { BigNumber } from "ethers"
+import { time } from "@nomicfoundation/hardhat-network-helpers"
 
 const contractName = "CastyToken"
 
-async function deployContractFixture() {}
+async function deployContractFixture() {
+  const contractFactory = await ethers.getContractFactory(contractName)
+  const contract = await contractFactory.deploy()
+  await contract.deployed()
+  return contract
+}
+
+const initialSupply = ethers.utils.parseEther("10000000000")
+const betweenMintsTime = 30 * 24 * 60 * 60
 
 describe("CastyToken", function () {
-  // TODO: implements
   describe("deploy", function () {
-    it("should initialize properly", async function () {})
+    it("should initialize properly", async function () {
+      const contract = await loadFixture(deployContractFixture)
+      expect(await contract.name()).to.equal("CastyToken")
+      expect(await contract.symbol()).to.equal("TY")
+      expect(await contract.decimals()).to.equal(18)
+      expect(await contract.totalSupply()).to.equal(initialSupply)
+    })
+    it("should mint initial supplied tokens to the owner", async function () {
+      const [owner] = await ethers.getSigners()
+      const contract = await loadFixture(deployContractFixture)
+      const totalSupply = await contract.totalSupply()
+
+      expect(await contract.balanceOf(owner.address)).to.equal(totalSupply)
+    })
+  })
+  describe("mint", function () {
+    it("mintable amount increase each 30 days", async function () {
+      const contract = await loadFixture(deployContractFixture)
+      const [owner] = await ethers.getSigners()
+      const amount = initialSupply.mul(2).div(100)
+
+      const mintTime = (await time.latest()) + betweenMintsTime
+      await time.increaseTo(mintTime)
+      expect(await contract.mintableAmount()).to.equal(amount)
+
+      await contract.mint(owner.address, amount)
+      expect(await contract.balanceOf(owner.address)).to.equal(initialSupply.add(amount))
+    })
+
+    it("should mint tokens to the owner", async function () {
+      const [owner, account1] = await ethers.getSigners()
+      const contract = await loadFixture(deployContractFixture)
+      const amount = initialSupply.mul(2).div(100)
+
+      const mintTime = (await time.latest()) + betweenMintsTime
+      await time.increaseTo(mintTime)
+
+      // not owner
+      await expect(
+        contract.connect(account1).mint(account1.address, amount)
+      ).to.be.revertedWithCustomError(contract, "OwnableUnauthorizedAccount")
+
+      // owner
+      await contract.mint(owner.address, amount)
+      expect(await contract.balanceOf(owner.address)).to.equal(initialSupply.add(amount))
+    })
   })
 })
